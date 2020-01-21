e78d60bdeb7cefa10bc5d3cbdfc066231e2d9583
SamSong <ymsonghuan@gmail.com>
2020-01-21 02:51:36 -0500

    add DTO

--- DatingApp/DatingApp.API/Controllers/AuthController.cs
+++ DatingApp/DatingApp.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,19 +16,19 @@ namespace DatingApp.API.Controllers
             _repo = repo;
         }
 
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if (await _repo.UserExists(username))
+            if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("User already exist");
 
             var userToCreate = new User
             {
-                Username = username
+                Username = userForRegisterDto.Username
             };
 
-            var createdUser = _repo.Register(userToCreate, password);
+            var createdUser = _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
